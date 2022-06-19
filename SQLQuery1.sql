fbc7f4486251b46c4b57b7243b6c635f858f9945
hem_b <hemanthb93@gmail.com>
2022-06-19 10:59:55 +0530

    [hemanth]:Edit the address book table details using Update

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -3,3 +3,12 @@
 --PhoneNo INTEGER PRIMARY KEY,Email VARCHAR(30))
 
 --INSERT INTO AddressBook(FirstName,LastName,Address,City,State,Zip,PhoneNo,Email) VALUES ('Hemanth','B','27,ashanivas','chennai','Tamil nadu',678876,980789,'hemanth@gm.com');
+
+--SELECT * FROM AddressBook;
+
+--INSERT INTO AddressBook VALUES('Vysagh','B','1,Kg Garden,3rd block','kovai','Tamil Nadu',600009,99887788,'vy@ga.in');
+--INSERT INTO AddressBook VALUES('jubith','k','1,jayes bhavan3rd block','kannur','Tamil Nadu',611009,99887766,'jubi@ga.in');
+
+--SELECT * FROM AddressBook WHERE Firstname = 'jubith';
+
+UPDATE AddressBook SET State = 'Kerala' WHERE FirstName = 'jubith';
