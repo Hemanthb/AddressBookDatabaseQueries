841320412df4988c051899db2866d652bc407236
hem_b <hemanthb93@gmail.com>
2022-06-19 10:43:02 +0530

    [Hemanth]:Created a Db AddressBook Service and a table

---
+++ SQLQuery1.sql
@@ -0,0 +1,3 @@
+--Use AddressBookService;
+--CREATE TABLE AddressBook (FirstName VARCHAR(20),LastName VARCHAR(20),Address VARCHAR(50),City VARCHAR(20),State VARCHAR(15),Zip INTEGER,
+--PhoneNo INTEGER PRIMARY KEY,Email VARCHAR(30))
