67129b62c5195bf50803b2614d5acec56e240119
hem_b <hemanthb93@gmail.com>
2022-06-19 10:47:06 +0530

    [Hemanth]:Insert elements into table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,5 @@
 --Use AddressBookService;
 --CREATE TABLE AddressBook (FirstName VARCHAR(20),LastName VARCHAR(20),Address VARCHAR(50),City VARCHAR(20),State VARCHAR(15),Zip INTEGER,
 --PhoneNo INTEGER PRIMARY KEY,Email VARCHAR(30))
+
+--INSERT INTO AddressBook(FirstName,LastName,Address,City,State,Zip,PhoneNo,Email) VALUES ('Hemanth','B','27,ashanivas','chennai','Tamil nadu',678876,980789,'hemanth@gm.com');
